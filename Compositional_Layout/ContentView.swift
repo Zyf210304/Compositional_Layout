283e6a16c57d95d444bfb94bce81ec1fd0050a0e
张亚飞 <672233580@qq.com>
2021-01-29 10:01:52 +0800

    Initial Commit

---
+++ Compositional_Layout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Compositional_Layout
+//
+//  Created by 亚飞 on 2021/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
